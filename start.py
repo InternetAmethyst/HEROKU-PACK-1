6e6ebe940ee2c35120fa61d36d7b30b61f127124
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 15:58:32 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
-system("python3 -m DYNAMIC")
+system ("git clone https://GitHub.com/TeamExtremePro/ExtremeProUserbot && cd ExtremeProUserbot && python3 -m amanpandey")
+system("python3 -m amanpandey")
