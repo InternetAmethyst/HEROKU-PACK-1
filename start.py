da80902221cf9e26ffc0a2f227f933e7a5b4ba08
GENIUS SAVAGE SHIV <83080405+SHIVGULSHAN@users.noreply.github.com>
2021-05-25 23:13:33 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/TeamDynamic/Dynamic-Userbot && cd Dynamic-Userbot && python3 -m DYNAMIC")
+system ("git clone https://github.com/SHIVGULSHAN/TANDAV-X-USERBOT && cd Dynamic-Userbot && python3 -m DYNAMIC")
 system("python3 -m DYNAMIC")
