6e8606ad5a166247f0305ff4e45be450a485ec0d
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-05-27 18:06:22 +0530

    Fixed By Aman Pandey Dont Remove Credites Else Get ready to get gift

--- start.py
+++ start.py
@@ -1,3 +1,3 @@
 from os import system # chala ja 
-system ("git clone https://github.com/SHIVGULSHAN/TANDAV-X-USERBOT && cd Dynamic-Userbot && python3 -m DYNAMIC")
+system ("git clone https://github.com/SHIVGULSHAN/TANDAV-X-USERBOT && cd TANDAV-X-USERBOT && python3 -m DYNAMIC")
 system("python3 -m DYNAMIC")
