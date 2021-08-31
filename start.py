3a82d160f36c755dac143ffaca9b9b1385c0af08
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-31 18:04:41 +0530

    kk

--- start.py
+++ start.py
@@ -1,3 +1,2 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/TeamExtremePro/ExtremeProUserbot && cd ExtremeProUserbot && python3 -m amanpandey")
-system("python3 -m amanpandey")
+system ("git clone https://github.com/SRIDHAR2021SIDDHARTH/DynamicUserbot && cd DynamicUserbot && python3 -m DYNAMIC")
